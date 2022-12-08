c0191f69354215ff0248e6e0af6154baf0038700
Drew Neece <dneece5@aggienetwork.com>
2022-12-08 14:48:25 -0600

    complete step 2, store cached element references

--- js/app.js
+++ js/app.js
@@ -11,7 +11,8 @@ let tie     //represents if the game has ended in a tie
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const squareEls = document.getElementsByClassName('sqr')
+const messageEl = document.getElementById('message')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -21,6 +22,7 @@ let tie     //represents if the game has ended in a tie
 
 
 
+/*------------------------------- Pseudo Code -------------------------------*/
 //// Step 1 - Define the required variables used to track the state of the game
 
   //// 1a) Use a variable named `board` to represent the state of the squares on
@@ -33,13 +35,13 @@ let tie     //represents if the game has ended in a tie
   //// 1d) Use a variable named `tie` to represent if the game has ended in a tie.
 
 
-// Step 2 - Store cached element references.
+//// Step 2 - Store cached element references.
 
-  // 2a) In a constant called `squareEls`, store the nine elements 
-  //    representing the squares on the page.
+  //// 2a) In a constant called `squareEls`, store the nine elements 
+  ////    representing the squares on the page.
 
-  // 2b) In a constant called `messageEl`, store the element that displays the 
-  //    game's status on the page.
+  //// 2b) In a constant called `messageEl`, store the element that displays the 
+  ////   game's status on the page.
 
 
 // Step 3 - Upon loading, the game state should be initialized, and a function 
